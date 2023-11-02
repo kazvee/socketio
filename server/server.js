2852224d96a41d54cff7eac0d1da67b7a1131567
kazvee <kayvee123@gmail.com>
2023-11-01 18:53:52 -0600

    Update server to use socketio instead of express

--- server/server.js
+++ server/server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
-
-const port = 9001;
-
 const app = express();
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
+
+const port = 9001;
+
+// Step 1 - Require Socket io
+const socketio = require('socket.io');
+
+// Step 2 - Require HTTPS
+const http = require('http');
+
+// Step 3 - Wrap the app in the HTTP
+const server = http.createServer(app);
+
+// Step 4 - Make sure you have io wrap over the server
+const io = socketio(server);
 
 app.get('/', (req, res) => {
   res.send('Welcome! 🌞');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`App is listening on port ${port}! 😃`);
 });
