dd270339693228d3d1f41ce2a68b6b4a5183a9ac
kazvee <kayvee123@gmail.com>
2023-11-01 20:22:06 -0600

    Set up socketio on the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 
+// Step 1 - Install client socket io
+
+// Step 2 - Import socket io
+import socketToClient from 'socket.io-client';
+
+// Step 3 - Declare your backend
+const ENDPOINT = 'http://localhost:9001/';
+
+// Step 4 - Connect!
+const connection = socketToClient(ENDPOINT);
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World! 🌍</h1>
-
-
     </div>
   );
 }
